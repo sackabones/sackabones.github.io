666bea68dd206f8826079f0edc189b7480d1f1bc
sackabones <50327415+sackabones@users.noreply.github.com>
2019-05-06 11:26:15 -0400

    Add files via upload
    add weapon loadout randomizer

--- randomizerScript.js
+++ randomizerScript.js
@@ -1,7 +1,16 @@
+//array to hold all possible legends
 var legends = new Array("Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", "Octane", "Player Choice");
+//array to hold major drop location
 var locations = new Array("Slum Lakes", "Artillery", "Relay", "Cascades", "The Pit", "Runoff", "Bunker", "Airbase", "Farm", "Wetlands", "Swamps", "Hydro Dam", "Bridges", "Repulsor", "Water Treatment", "Market", "Skull Town", "Thunderdome", "Drop Ship", "Player Choice");
+//array to hold all the common weapons
+var weapons = new Array("Longbow", "G7 Scout", "Triple Take", "Hemlok", "R-301", "Flatline", "Havoc", "Spitfire", "Devotion", "R-99", "Prowler", "Alternator", "EVA-8", "Peacekeeper", "Mozambique", "Wingman", "RE-45", "P2020");
+//string to hold output message
+var playerOneOutput = "";
+var playerTwoOutput = "";
+var playerThreeOutput = "";
 
 function randomize(){
+
 	//random the players and drop location
 	var randLeg1 = Math.floor(Math.random()*10);
 	var randLeg2 = Math.floor(Math.random()*10);
@@ -23,24 +32,55 @@ function randomize(){
 			randLeg3 = Math.floor(Math.random()*10);
 		}
 	}
+	//make sure that each player does not have duplicate weapons
+
+
 
 	//check how many players to random and output
 	if(document.getElementById("onePlayer").checked){
-		document.getElementById("player1").innerHTML = "Player 1: "+legends[randLeg1];
-		document.getElementById("player2").innerHTML = "";
-		document.getElementById("player3").innerHTML = "";
-		document.getElementById("dropLoc").innerHTML = "Drop location: "+locations[randLoc];
+		playerOneOutput = "Player 1: "+legends[randLeg1];
+		playerTwoOutput = "";
+		playerThreeOutput = "";
+		if(document.getElementById("randomizeLoadout").checked){
+			playerOneOutput += randWeapons();
+		}
+		printInfo(randLoc);
 	}
 	if(document.getElementById("twoPlayer").checked){
-		document.getElementById("player1").innerHTML = "Player 1: "+legends[randLeg1];
-		document.getElementById("player2").innerHTML = "Player 2: "+legends[randLeg2];
-		document.getElementById("player3").innerHTML = "";
-		document.getElementById("dropLoc").innerHTML = "Drop location: "+locations[randLoc];
+		playerOneOutput = "Player 1: "+legends[randLeg1];
+		playerTwoOutput = "Player 2: "+legends[randLeg2];
+		playerThreeOutput = "";
+		if(document.getElementById("randomizeLoadout").checked){
+			playerOneOutput += randWeapons();
+			playerTwoOutput += randWeapons();
+		}
+		printInfo(randLoc);
 	}
 	if(document.getElementById("threePlayer").checked){
-		document.getElementById("player1").innerHTML = "Player 1: "+legends[randLeg1];
-		document.getElementById("player2").innerHTML = "Player 2: "+legends[randLeg2];
-		document.getElementById("player3").innerHTML = "Player 3: "+legends[randLeg3];
-		document.getElementById("dropLoc").innerHTML = "Drop location: "+locations[randLoc];
+		playerOneOutput = "Player 1: "+legends[randLeg1];
+		playerTwoOutput = "Player 2: "+legends[randLeg2];
+		playerThreeOutput = "Player 3: "+legends[randLeg3];
+		if(document.getElementById("randomizeLoadout").checked){
+			playerOneOutput += randWeapons();
+			playerTwoOutput += randWeapons();
+			playerThreeOutput += randWeapons();
+		}
+		printInfo(randLoc);
 	}
+}
+
+function randWeapons(){
+	var randWep1 = Math.floor(Math.random()*18);
+	var randWep2 = Math.floor(Math.random()*18);
+	while(randWep1 == randWep2){
+		randWep2 = Math.floor(Math.random()*18);
+	}
+	return " | "+weapons[randWep1]+", "+weapons[randWep2];
+}
+
+function printInfo(randLoc){
+	document.getElementById("player1").innerHTML = playerOneOutput;
+	document.getElementById("player2").innerHTML = playerTwoOutput;
+	document.getElementById("player3").innerHTML = playerThreeOutput;
+	document.getElementById("dropLoc").innerHTML = "Drop location: "+locations[randLoc];
 }
