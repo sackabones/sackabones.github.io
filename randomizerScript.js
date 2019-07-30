cb11a8a248043fcec50523c1ab7e51a50cbdfe8a
sackabones <50327415+sackabones@users.noreply.github.com>
2019-07-30 19:05:46 -0400

    Update files via upload
    Replace index.html and randomizerScript.js to add Wattson, new landing locations, and text.

--- randomizerScript.js
+++ randomizerScript.js
@@ -1,7 +1,7 @@
 //array to hold all possible legends
-var legends = new Array("Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", "Octane", "Player Choice");
+var legends = new Array("Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", "Octane", "Wattson", "Player Choice");
 //array to hold major drop location
-var locations = new Array("Slum Lakes", "Artillery", "Relay", "Cascades", "The Pit", "Runoff", "Bunker", "Airbase", "Farm", "Wetlands", "Swamps", "Hydro Dam", "Bridges", "Repulsor", "Water Treatment", "Market", "Skull Town", "Thunderdome", "Drop Ship", "Player Choice");
+var locations = new Array("Slum Lakes", "Artillery", "Relay", "Containment", "The Pit", "Runoff", "Bunker", "Airbase", "Farm", "Wetlands", "Swamps", "Hydro Dam", "The Cage", "Repulsor", "Water Treatment", "Market", "Skull Town", "Thunderdome", "Drop Ship", "Nesting Grounds", "Player Choice");
 //array to hold all the common weapons
 var weapons = new Array("Longbow", "G7 Scout", "Triple Take", "Hemlok", "R-301", "Flatline", "Havoc", "Spitfire", "Devotion", "R-99", "Prowler", "Alternator", "EVA-8", "Peacekeeper", "Mozambique", "Wingman", "RE-45", "P2020");
 //string to hold output message
@@ -12,24 +12,24 @@ var playerThreeOutput = "";
 function randomize(){
 
 	//random the players and drop location
-	var randLeg1 = Math.floor(Math.random()*10);
-	var randLeg2 = Math.floor(Math.random()*10);
-	var randLeg3 = Math.floor(Math.random()*10);
+	var randLeg1 = Math.floor(Math.random()*legends.length);
+	var randLeg2 = Math.floor(Math.random()*legends.length);
+	var randLeg3 = Math.floor(Math.random()*legends.length);
 	//var randLoc = Math.floor(Math.random()*20);
 	//make sure there are no duplicate legends randomed, "Player Choice" can be duped.
 	if(randLeg1 == randLeg2 && (legends[randLeg1].localeCompare("Player Choice") != 0)){
 		while(randLeg1 == randLeg2){
-			randLeg2 = Math.floor(Math.random()*10);
+			randLeg2 = Math.floor(Math.random()*legends.length);
 		}
 	}
 	if(randLeg1 == randLeg3 && (legends[randLeg1].localeCompare("Player Choice") != 0)){
 		while(randLeg1 == randLeg3){
-			randLeg3 = Math.floor(Math.random()*10);
+			randLeg3 = Math.floor(Math.random()*legends.length);
 		}
 	}
 	if(randLeg2 == randLeg3 && (legends[randLeg2].localeCompare("Player Choice") != 0)){
 		while(randLeg2 == randLeg3){
-			randLeg3 = Math.floor(Math.random()*10);
+			randLeg3 = Math.floor(Math.random()*legends.length);
 		}
 	}
 	//make sure that each player does not have duplicate weapons
@@ -73,10 +73,11 @@ function randomize(){
 }
 
 function randWeapons(){
-	var randWep1 = Math.floor(Math.random()*18);
-	var randWep2 = Math.floor(Math.random()*18);
+	var randWep1 = Math.floor(Math.random()*weapons.length);
+	var randWep2 = Math.floor(Math.random()*weapons.length);
+	var randWep2 = Math.floor(Math.random()*weapons.length);
 	while(randWep1 == randWep2){
-		randWep2 = Math.floor(Math.random()*18);
+		randWep2 = Math.floor(Math.random()*weapons.length);
 	}
 	return " | "+weapons[randWep1]+", "+weapons[randWep2];
 }
@@ -91,7 +92,7 @@ function printLeg(){
 }
 
 function checkAndPrintLoc(){
-	var randLoc = Math.floor(Math.random()*20);
+	var randLoc = Math.floor(Math.random()*locations.length);
 	if(document.getElementById("randomizeLocation").checked){
 		document.getElementById("dropLoc").innerHTML = "Drop location: "+locations[randLoc];
 	}
