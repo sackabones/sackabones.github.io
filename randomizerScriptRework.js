0d835844a3a929a742e2c2e174c771d2a6d3105f
sackabones <50327415+sackabones@users.noreply.github.com>
2020-02-12 09:42:55 -0500

    Season 4 update 2
    Add second set of locations for World's Edge map. Save old location list for easy swapping between the two loc sets

--- randomizerScriptRework.js
+++ randomizerScriptRework.js
@@ -1,7 +1,11 @@
 //array to hold all possible legends
 var legends = new Array("Player Choice", "Bloodhound", "Gibraltar", "Lifeline", "Pathfinder", "Wraith", "Bangalore", "Caustic", "Mirage", "Octane", "Wattson", "Crypto", "Revenant");
 //array to hold major drop location
-var locations = new Array("Slum Lakes", "Artillery", "Relay", "Containment", "The Pit", "Runoff", "Bunker", "Airbase", "Farm", "Wetlands", "Swamps", "Hydro Dam", "The Cage", "Repulsor", "Water Treatment", "Market", "Skull Town", "Thunderdome", "Drop Ship", "Nesting Grounds", "Player Choice", "Gauntlet");
+//choose which set of locations to use by commenting out/in the var the corresponds to the map you are playing, King's Canyon or World's Edge
+//King's Canyon (below)
+//var locations = new Array("Slum Lakes", "Artillery", "Relay", "Containment", "The Pit", "Runoff", "Bunker", "Airbase", "Farm", "Wetlands", "Swamps", "Hydro Dam", "The Cage", "Repulsor", "Water Treatment", "Market", "Skull Town", "Thunderdome", "Drop Ship", "Nesting Grounds", "Player Choice", "Gauntlet");
+//World's Edge (below)
+var locations = new Array("Skyhook", "Survey Camp", "Refinery", "The Epicenter", "Drill Site", "Lava Fissure", "The Train Yard", "Fragment West", "Fragment East", "Overlook", "The Geyser", "Harvester", "Mirage Voyage", "Terminal Station", "The Tree", "Sorting Factory", "Lava City", "The Dome", "Train", "Player Choice");
 //array to hold all the common weapons
 var weapons = new Array("Longbow", "Sentinel", "G7 Scout", "Triple Take", "Hemlok", "R-301", "Flatline", "Havoc", "Spitfire", "L-Star", "R-99", "Prowler", "Alternator", "EVA-8", "Peacekeeper", "Mozambique", "Wingman", "RE-45", "P2020");
 //string to hold output message
